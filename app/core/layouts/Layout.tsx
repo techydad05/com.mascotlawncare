561adca43ca45c70370823be28fde7a0b7bb51e7
techydad05 <techydad05@gmail.com>
2021-02-25 06:23:47 -0500

    added image gallery and lightbox

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -12,8 +12,12 @@ const Layout = ({ title, children }: LayoutProps) => {
       <Head>
         <title>{title || "com.mascotlawncare"}</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
-
       {children}
     </>
   )
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -10,22 +10,42 @@ import { ErrorBoundary } from "react-error-boundary"
 import { queryCache } from "react-query"
 import LoginForm from "app/auth/components/LoginForm"
 
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import React from "react"
+
+import SimpleReactLightbox from "simple-react-lightbox"
+
+//You can customize this as you want and even move it out to a separate file
+const theme = createMuiTheme({
+  palette: {
+    type: "light",
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
   const router = useRouter()
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      resetKeys={[router.asPath]}
-      onReset={() => {
-        // This ensures the Blitz useQuery hooks will automatically refetch
-        // data any time you reset the error boundary
-        queryCache.resetErrorBoundaries()
-      }}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        resetKeys={[router.asPath]}
+        onReset={() => {
+          // This ensures the Blitz useQuery hooks will automatically refetch
+          // data any time you reset the error boundary
+          queryCache.resetErrorBoundaries()
+        }}
+      >
+        {getLayout(
+          <SimpleReactLightbox>
+            <Component {...pageProps} />
+          </SimpleReactLightbox>
+        )}
+      </ErrorBoundary>
+    </ThemeProvider>
   )
 }
 
